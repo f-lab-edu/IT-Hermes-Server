62ea1649da8e02b49d0932b72998c982175f4515
eunyoung23 <eun970923@naver.com>
2022-12-16 01:05:00 +0900

    Add: 프로젝트 파일 추가

---
+++ src/main/java/com/hermes/ithermes/ItHermesApplication.java
@@ -0,0 +1,13 @@
+package com.hermes.ithermes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItHermesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItHermesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hermes/ithermes/ItHermesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hermes.ithermes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItHermesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
