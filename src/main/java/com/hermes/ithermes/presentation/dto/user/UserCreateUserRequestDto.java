8e4bc237225a649ec0251cc3c5c7eaff60c93bd4
kimseungki <skkim0832@naver.com>
2023-01-17 02:32:50 +0900

    Factor : 리펙토링 비밀번호 유효성 변경 (#23)

--- src/main/java/com/hermes/ithermes/presentation/dto/user/UserCreateUserRequestDto.java
+++ src/main/java/com/hermes/ithermes/presentation/dto/user/UserCreateUserRequestDto.java
@@ -7,13 +7,17 @@ import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 
 @Getter
+@RequiredArgsConstructor
 public class UserCreateUserRequestDto {
     @NonNull
     @NotBlank(message = "아이디는 필수 입력사항 입니다.")
     private String id;
+    @NonNull
+    @NotBlank(message = "비밀번호는 필수 입력사항 입니다.")
     @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,20}",
             message = "비밀번호는 영문 대,소문자와 숫자, 특수기호가 적어도 1개 이상씩 포함된 8자 ~ 20자의 비밀번호여야 합니다.")
     private String password;
