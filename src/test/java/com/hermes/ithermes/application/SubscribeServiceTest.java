27a2b3e4d9b1426bba7ceb62857449bdfaf13920
kimseungki <skkim0832@naver.com>
2023-01-17 17:46:52 +0900

    Test : 구독 테스트 코드를 구현한다 (#16)

--- src/test/java/com/hermes/ithermes/application/SubscribeServiceTest.java
+++ src/test/java/com/hermes/ithermes/application/SubscribeServiceTest.java
@@ -1,7 +1,120 @@
 package com.hermes.ithermes.application;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.hermes.ithermes.domain.entity.ContentsProvider;
+import com.hermes.ithermes.domain.entity.Subscribe;
+import com.hermes.ithermes.domain.entity.User;
+import com.hermes.ithermes.domain.exception.WrongIdOrPasswordException;
+import com.hermes.ithermes.domain.factory.SubscribeFactory;
+import com.hermes.ithermes.domain.util.ActiveType;
+import com.hermes.ithermes.domain.util.CategoryType;
+import com.hermes.ithermes.domain.util.ContentsProviderType;
+import com.hermes.ithermes.domain.util.JobType;
+import com.hermes.ithermes.infrastructure.SubscribeRepository;
+import com.hermes.ithermes.presentation.dto.CommonResponseDto;
+import com.hermes.ithermes.presentation.dto.subscribe.SubscribeFindSubscribeRequestDto;
+import com.hermes.ithermes.presentation.dto.subscribe.SubscribeFindSubscribeResponseDto;
+import com.hermes.ithermes.presentation.dto.subscribe.SubscribePutSubscribeRequestDto;
+import com.hermes.ithermes.presentation.dto.user.UserCreateUserRequestDto;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 class SubscribeServiceTest {
+    @InjectMocks
+    private SubscribeService subscribeService;
+    @Mock
+    private SubscribeRepository subscribeRepository;
+    @Mock
+    private SubscribeFactory subscribeFactory;
 
+    private UserCreateUserRequestDto userCreateUserRequestDto;
+    private Subscribe subscribe;
+    private User user;
+    private ContentsProvider contentsProvider;
+
+    @BeforeEach
+    void setUp() {
+        user = User.builder()
+                .loginId("test").nickname("김승기")
+                .password("test1234").job(JobType.BACKEND)
+                .yearOfExperience(1)
+                .build();
+        contentsProvider = ContentsProvider.builder()
+                .category(CategoryType.JOB)
+                .name(ContentsProviderType.SARAMIN)
+                .build();
+
+        subscribe = Subscribe.builder()
+                .user(user)
+                .contentsProvider(contentsProvider)
+                .minYearOfExperience(3)
+                .maxYearOfExperience(5)
+                .job(JobType.BACKEND)
+                .isActive(ActiveType.ACTIVE)
+                .build();
+    }
+
+    @Test
+    @DisplayName("회원가입_정상처리")
+    void 구독_PUT_정상처리() {
+        SubscribePutSubscribeRequestDto subscribePutSubscribeRequestDto = new SubscribePutSubscribeRequestDto("test",
+                new String[]{"ACTIVE", "ACTIVE", "NOT_ACTIVE", "NOT_ACTIVE", "NOT_ACTIVE", "NOT_ACTIVE", "NOT_ACTIVE"},
+                JobType.BACKEND,
+                "3",
+                "5");
+        assertEquals(new CommonResponseDto().getMessage(), subscribeService.putSubscribe(subscribePutSubscribeRequestDto).getMessage());
+    }
+
+    @Test
+    @DisplayName("구독_PUT_실패처리")
+    void 구독_PUT_실패처리() {
+        SubscribePutSubscribeRequestDto subscribePutSubscribeRequestDto = new SubscribePutSubscribeRequestDto("test",
+                new String[]{"ACTIVE", "ACTIVE", "NOT_ACTIVE", "NOT_ACTIVE", "NOT_ACTIVE", "NOT_ACTIVE", "NOT_ACTIVE"},
+                JobType.BACKEND,
+                "3",
+                "5");
+
+        when(subscribeFactory.parsePutSubscribeDtoToSubscribes(any())).thenThrow(new WrongIdOrPasswordException());
+
+        assertThrows(WrongIdOrPasswordException.class, () -> subscribeService.putSubscribe(subscribePutSubscribeRequestDto).getMessage());
+    }
+
+    @Test
+    @DisplayName("구독조회_정상처리")
+    void 구독조회_정상처리() {
+        SubscribeFindSubscribeRequestDto subscribeFindSubscribeRequestDto = new SubscribeFindSubscribeRequestDto("test");
+        when(subscribeFactory.findJobCategoryData(any())).thenReturn(subscribe);
+        List<String> list = Arrays.asList("ACTIVE", "ACTIVE", "NOT_ACTIVE", "NOT_ACTIVE", "NOT_ACTIVE", "NOT_ACTIVE", "NOT_ACTIVE");
+        when(subscribeFactory.findActiveContentsProviderType(any()))
+                .thenReturn(list);
+
+        SubscribeFindSubscribeResponseDto subscribe = subscribeService.findSubscribe(subscribeFindSubscribeRequestDto);
+        assertEquals(JobType.BACKEND.name(), subscribe.getJob());
+        assertEquals("3", subscribe.getMinYearOfExperience());
+        assertEquals("5", subscribe.getMaxYearOfExperience());
+        for (int i = 0; i < list.size(); i++) {
+            assertEquals(list.get(i), subscribe.getKeywordList().get(i));
+        }
+    }
+
+    @Test
+    @DisplayName("구독조회_실패처리")
+    void 구독조회_실패처리() {
+        SubscribeFindSubscribeRequestDto subscribeFindSubscribeRequestDto = new SubscribeFindSubscribeRequestDto("test");
+        when(subscribeFactory.parseFindSubscribeDtoToSubscribes(any())).thenThrow(new WrongIdOrPasswordException());
+        assertThrows(WrongIdOrPasswordException.class, () -> subscribeService.findSubscribe(subscribeFindSubscribeRequestDto));
+    }
 }
--- src/test/java/com/hermes/ithermes/presentation/controller/SubscribeControllerTest.java
+++ src/test/java/com/hermes/ithermes/presentation/controller/SubscribeControllerTest.java
@@ -2,10 +2,23 @@ package com.hermes.ithermes.presentation.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hermes.ithermes.application.SubscribeService;
+import com.hermes.ithermes.domain.exception.WrongIdOrPasswordException;
+import com.hermes.ithermes.domain.util.JobType;
+import com.hermes.ithermes.presentation.dto.subscribe.SubscribePutSubscribeRequestDto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = SubscribeController.class)
 class SubscribeControllerTest {
 
     @MockBean
@@ -16,4 +29,36 @@ class SubscribeControllerTest {
 
     @Autowired
     ObjectMapper objectMapper;
+
+    @Test
+    @DisplayName("구독_PUT_정상처리")
+    void 구독_PUT_정상처리() throws Exception {
+        SubscribePutSubscribeRequestDto subscribePutSubscribeRequestDto = new SubscribePutSubscribeRequestDto("test",
+                        new String[]{"ACTIVE","ACTIVE","NOT_ACTIVE","NOT_ACTIVE","NOT_ACTIVE","NOT_ACTIVE","NOT_ACTIVE"},
+                        JobType.BACKEND,
+                        "3",
+                        "5");
+
+        mockMvc.perform(put("/subscribe/")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(subscribePutSubscribeRequestDto)))
+                .andExpect(status().isOk()); // 추후 201로 수정필요!
+    }
+
+    @Test
+    @DisplayName("구독_PUT_실패처리")
+    void 구독_PUT_실패처리() throws Exception {
+        SubscribePutSubscribeRequestDto subscribePutSubscribeRequestDto = new SubscribePutSubscribeRequestDto("test",
+                new String[]{"ACTIVE","ACTIVE","NOT_ACTIVE","NOT_ACTIVE","NOT_ACTIVE","NOT_ACTIVE","NOT_ACTIVE"},
+                JobType.BACKEND,
+                "3",
+                "5");
+
+        when(subscribeService.putSubscribe(any())).thenThrow(new WrongIdOrPasswordException());
+
+        mockMvc.perform(put("/subscribe/")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(subscribePutSubscribeRequestDto)))
+                .andExpect(status().isBadRequest());
+    }
 }
