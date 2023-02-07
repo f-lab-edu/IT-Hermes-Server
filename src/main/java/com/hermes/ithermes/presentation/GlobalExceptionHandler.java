58bb02e995e8bda1e85c3b0030a4c1fe57ed2a58
kimseungki <skkim0832@naver.com>
2023-02-07 10:17:27 +0900

    Fix : globalHandler unsafe 이슈로 인해 수정

--- src/main/java/com/hermes/ithermes/presentation/GlobalExceptionHandler.java
+++ src/main/java/com/hermes/ithermes/presentation/GlobalExceptionHandler.java
@@ -11,7 +11,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(NotExistsRequestParamException.class)
     public ResponseEntity<String> NotExistsRequestValue(){
-        return new ResponseEntity("잘못된 요청 파라미터 값입니다.", HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>("잘못된 요청 파라미터 값입니다.", HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(SameIdException.class)
