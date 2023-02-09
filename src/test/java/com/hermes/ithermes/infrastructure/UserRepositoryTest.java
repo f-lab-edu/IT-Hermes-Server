5a132f60f75b28668bbd73dbd90f68fb6ff73433
eunyoung23 <eun970923@naver.com>
2023-02-09 15:51:38 +0900

    Fix: userrepository 수정

--- src/test/java/com/hermes/ithermes/infrastructure/UserRepositoryTest.java
+++ src/test/java/com/hermes/ithermes/infrastructure/UserRepositoryTest.java
@@ -2,6 +2,7 @@ package com.hermes.ithermes.infrastructure;
 
 import com.hermes.ithermes.domain.entity.User;
 import com.hermes.ithermes.domain.util.JobType;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,10 +18,14 @@ class UserRepositoryTest {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private UserKeywordRegistryRepository userKeywordRegistryRepository;
+
     @Test
     @DisplayName("telegramId값이 null이 아닌 유저가 1명인지 테스트")
     void notExistsTelegramIdUserTest(){
         userRepository.deleteAll();
+        userKeywordRegistryRepository.deleteAll();
 
         User user1 = new User(1l,"회원1","login1","1234", JobType.BACKEND,1,"telegram아이디",false,"eysdfetwetwe");
         User user2 = new User(2l,"회원2","login2","5678", JobType.FRONT,0,null,false,"eysdfetwetwe");
@@ -28,7 +33,7 @@ class UserRepositoryTest {
         userRepository.save(user1);
         userRepository.save(user2);
 
-        assertEquals(1, userRepository.findByTelegramIdIsNotNull().size());
+        Assertions.assertEquals(1, userRepository.findByTelegramIdIsNotNull().size());
     }
 
 }
