6383589201ea8420d577053df5e458f4c68635db
kimseungki <skkim0832@naver.com>
2023-03-08 15:09:36 +0900

    Refactor : lettuce jedis로 수정 #53

--- src/main/java/com/hermes/ithermes/RedisConfig.java
+++ src/main/java/com/hermes/ithermes/RedisConfig.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
@@ -14,16 +14,16 @@ public class RedisConfig {
     @Value("${spring.redis.ipAddress}")
     private String ipAddress;
 
-    LettuceConnectionFactory lettuceConnectionFactory() {
-        log.info("[IP Address Check] : {}", ipAddress);
+    JedisConnectionFactory jedisConnectionFactory() {
+        log.info("[Redis IP Address Check] : {}", ipAddress);
         RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(ipAddress, 6379);
-        return new LettuceConnectionFactory(redisStandaloneConfiguration);
+        return new JedisConnectionFactory(redisStandaloneConfiguration);
     }
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate() {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(lettuceConnectionFactory());
+        template.setConnectionFactory(jedisConnectionFactory());
         return template;
     }
 
