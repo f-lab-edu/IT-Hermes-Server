5d77c6fb250ce068d7d003707928ff4249f66b75
kimseungki <skkim0832@naver.com>
2023-03-08 16:28:30 +0900

    Refactor : Redis 부분 수정 #53

--- src/main/java/com/hermes/ithermes/RedisConfig.java
+++ src/main/java/com/hermes/ithermes/RedisConfig.java
@@ -6,19 +6,21 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 @Slf4j
+@EnableRedisRepositories
 public class RedisConfig {
     @Value("${spring.redis.ipAddress}")
     private String ipAddress;
 
+    @Bean
     LettuceConnectionFactory lettuceConnectionFactory() {
         log.info("[lettuce ip 설정 확인]: {}", ipAddress);
         LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(ipAddress,6379);
-        lettuceConnectionFactory.afterPropertiesSet();
         return lettuceConnectionFactory;
     }
 
