e72656bed35d94a6fb5c6ba0531a0d61f77d563f
kimseungki <skkim0832@naver.com>
2023-03-08 14:58:03 +0900

    Refactor : Redis 로그확인 #53

--- src/main/java/com/hermes/ithermes/RedisConfig.java
+++ src/main/java/com/hermes/ithermes/RedisConfig.java
@@ -1,5 +1,6 @@
 package com.hermes.ithermes;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,11 +9,13 @@ import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactor
 import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
+@Slf4j
 public class RedisConfig {
     @Value("${spring.redis.ipAddress}")
     private String ipAddress;
 
     LettuceConnectionFactory lettuceConnectionFactory() {
+        log.info("[IP Address Check] : {}", ipAddress);
         RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(ipAddress, 6379);
         return new LettuceConnectionFactory(redisStandaloneConfiguration);
     }
