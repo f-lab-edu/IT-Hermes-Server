75e785d3d4b2e41448f17cdb1fa40765d1b8bc2d
eunyoung23 <eun970923@naver.com>
2023-03-19 18:20:32 +0900

    Fix: Elasticsearch 관련 파일 삭제

--- src/main/java/com/hermes/ithermes/ElasticsearchOperations.java
+++
@@ -1,7 +0,0 @@
-package com.hermes.ithermes;
-
-import org.springframework.data.elasticsearch.core.DocumentOperations;
-import org.springframework.data.elasticsearch.core.SearchOperations;
-
-public interface ElasticsearchOperations extends DocumentOperations, SearchOperations {
-}
