594167a1004807c91a266f30faa590c0ecd0acbe
kimseungki <skkim0832@naver.com>
2023-04-04 01:09:35 +0900

    Refactor : Reporitory 테스트코드 이슈 해결

--- src/test/java/com/hermes/ithermes/presentation/controller/UserControllerTest.java
+++ src/test/java/com/hermes/ithermes/presentation/controller/UserControllerTest.java
@@ -12,6 +12,7 @@ import com.hermes.ithermes.presentation.dto.user.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
@@ -26,6 +27,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(controllers = UserController.class)
+@AutoConfigureDataJpa
 class UserControllerTest {
 
     @MockBean
